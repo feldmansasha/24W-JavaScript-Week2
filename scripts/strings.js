4d79070c9b342c0a833799160ad7c4d2d5feea07
feldmansasha <feldmansasha5@gmail.com>
2024-01-15 20:22:16 -0500

    strings mid changes

---
+++ scripts/strings.js
@@ -0,0 +1,20 @@
+var output = document.querySelector("p");
+output.textContent = 10;
+    /* STEP 1: Creating Strings */
+    var string1="Hello! Howdy?";
+    output.textContent=string1;
+    // Note - whether you use 'single' or "double" quotes is a personal preference - try to be consistent
+
+    /* STEP 2: Escaping Characters */
+    var string2='It\'s cold outside';
+    output.textContent=string2;
+
+    /* STEP 3: Concatenation */
+
+    /* STEP 4: Numbers and Strings */
+
+    // numbers can be converted to strings
+
+    // strings can be converted to numbers, too
+
+    // and back again, if we want
