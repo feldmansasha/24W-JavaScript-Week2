d3ba14a71eaff1077f7283050a06dce10d480cec
feldmansasha <feldmansasha5@gmail.com>
2024-01-15 20:38:04 -0500

    step 3

--- scripts/methods.js
+++ scripts/methods.js
@@ -1,16 +1,19 @@
 var output = document.querySelector("p");
-    var myString = "Hello! How are you are?";
-    output.textContent=myString;
-    /* STEP 1: String Length */
+var myString = "Hello! How are you are?";
+output.textContent=myString;
+/* STEP 1: String Length */
+output.textContent=myString.length;
 
-    /* STEP 2: Retrieving a Specific String Character */
+/* STEP 2: Retrieving a Specific String Character */
+output.textContent=myString[4];
+output.textContent=myString[9];
+    
+/* STEP 3: Extracting Part of a String */
+output.textContent=myString.indexOf("are");
+// Note - if the substring is not found within the string, indexOf() returns -1
 
-    /* STEP 3: Extracting Part of a String */
+// Note - if you don't specify where to end the slice, the method returns the rest of the string
 
-    // Note - if the substring is not found within the string, indexOf() returns -1
+/* STEP 4: Changing Case */
 
-    // Note - if you don't specify where to end the slice, the method returns the rest of the string
-
-    /* STEP 4: Changing Case */
-
-    /* STEP 5: Updating Parts of a String */
+/* STEP 5: Updating Parts of a String */
--- scripts/strings.js
+++ scripts/strings.js
@@ -1,30 +1,30 @@
 var output = document.querySelector("p");
 output.textContent = 10;
-    /* STEP 1: Creating Strings */
-    var string1="Hello! Howdy?";
-    output.textContent=string1;
-    // Note - whether you use 'single' or "double" quotes is a personal preference - try to be consistent
+/* STEP 1: Creating Strings */
+var string1="Hello! Howdy?";
+output.textContent=string1;
+// Note - whether you use 'single' or "double" quotes is a personal preference - try to be consistent
 
-    /* STEP 2: Escaping Characters */
-    var string2="It's cold outside";
-    output.textContent=string2;
+/* STEP 2: Escaping Characters */
+var string2="It's cold outside";
+output.textContent=string2;
 
-    /* STEP 3: Concatenation */
-    output.textContent=string1+" "+string2+20;
-    output.textContent=20+20; // output 40
-    output.textContent=20+"20"; // output 20 20
+/* STEP 3: Concatenation */
+output.textContent=string1+" "+string2+20;
+output.textContent=20+20; // output 40
+output.textContent=20+"20"; // output 20 20
 
-    /* STEP 4: Numbers and Strings */
+/* STEP 4: Numbers and Strings */
 
-    // numbers can be converted to strings
-    output.textContent="ABC"+30; //output ABC30
+// numbers can be converted to strings
+output.textContent="ABC"+30; //output ABC30
 
-    // strings can be converted to numbers, too
-    var num1=40;
-    var num2=50;
-    output.textContent=num1.toString()+String(num2);
+// strings can be converted to numbers, too
+var num1=40;
+var num2=50;
+output.textContent=num1.toString()+String(num2);
 
-    // and back again, if we want
-    var num3="20";
-    var num4=Number(num3);
-    console.log(typeof(num4));
+// and back again, if we want
+var num3="20";
+var num4=Number(num3);
+console.log(typeof(num4));
