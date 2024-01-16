2e6ada7ad524aa4501c652a17d2cb32693668db6
feldmansasha <feldmansasha5@gmail.com>
2024-01-15 20:42:43 -0500

    final

--- scripts/methods.js
+++ scripts/methods.js
@@ -10,10 +10,17 @@ output.textContent=myString[9];
     
 /* STEP 3: Extracting Part of a String */
 output.textContent=myString.indexOf("are");
+output.textContent=myString.indexOf("is");
+
 // Note - if the substring is not found within the string, indexOf() returns -1
+output.textContent=myString.slice(4, 8);
+output.textContent=myString.slice(4);
 
 // Note - if you don't specify where to end the slice, the method returns the rest of the string
 
 /* STEP 4: Changing Case */
+output.textContent=myString.toUpperCase();
+output.textContent=myString.toLowerCase();
 
 /* STEP 5: Updating Parts of a String */
+output.textContent=myString.replace("are", "REPLACED");
